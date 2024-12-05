62d273d9647738016157de1ef1e61be29628b645
NhiDAG <amijeon2019@gmail.com>
2024-12-05 18:49:33 +0700

    FirstCommit

---
+++ src/main/java/function/demo_api/Demo_API.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package function.demo_api;
+
+/**
+ *
+ * @author ASUS ZenBook
+ */
+public class Demo_API {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
